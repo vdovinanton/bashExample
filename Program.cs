fae3521883d1731b19c21b66cd6e5ba82ab0451a
anton vdovin <antonvdovin92@gmail.com>
2018-03-07 10:52:42 +0200

    removed dead code

--- Program.cs
+++ Program.cs
@@ -22,29 +22,4 @@ namespace CoreApp
             Console.ReadKey();
         }
     }
-
-    public static class ShellHelper
-    {
-        public static string Bash(this string cmd)
-        {
-            var escapedArgs = cmd.Replace("\"", "\\\"");
-
-            var process = new Process()
-            {
-                StartInfo = new ProcessStartInfo
-                {
-                    FileName = "/bin/bash",
-                    Arguments = $"-c \"{escapedArgs}\"",
-                    RedirectStandardOutput = true,
-                    UseShellExecute = false,
-                    CreateNoWindow = true,
-                }
-            };
-            process.Start();
-            string result = process.StandardOutput.ReadToEnd();
-            process.WaitForExit();
-            return result;
-        }
-    }
-
 }
