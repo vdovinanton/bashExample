99018838879837c58a7829c80ad54c9d256f10e4
anton vdovin <antonvdovin92@gmail.com>
2018-03-07 10:51:14 +0200

    cleanup

--- Program.cs
+++ Program.cs
@@ -15,49 +15,9 @@ namespace CoreApp
             var file = new FileService();
             var bash = new MacService();
 
-            var manager = new CommandManager(bash, file);
+            var console = new CommandManager(bash, file);
 
-            manager.Connect();
-
-            #region unusage
-            //var fileName = "sc3.txt";
-            //var rootDirectory = Directory.GetCurrentDirectory();
-            //var filePath = $"{Directory.GetCurrentDirectory()}//{fileName}";
-
-
-            //Console.WriteLine(rootDirectory);
-            //Console.WriteLine(filePath);
-
-            //var isFile = File.Exists(filePath);
-            //Console.WriteLine(isFile);
-            //if (!isFile)
-            //    using (StreamWriter writer = new StreamWriter(fileName))
-            //    {
-            //        //writer.NewLine = "\n";
-            //        writer.NewLine = Environment.NewLine;
-            //        writer.WriteLine("#!/bin/bash");
-            //        writer.WriteLine("osascript <<EOF");
-            //        writer.WriteLine("tell application \"System Events\"");
-            //        writer.WriteLine("tell current location of network preferences");
-            //        writer.WriteLine("set myVPN to the service \"UniVPN\"");
-            //        writer.WriteLine("if myVPN is not null then");
-            //        writer.WriteLine("if current configuration of myVPN is not connected then");
-            //        writer.WriteLine("connect myVPN");
-            //        writer.WriteLine("end if");
-            //        writer.WriteLine("end if");
-            //        writer.WriteLine("end tell");
-            //        writer.WriteLine("return 120");
-            //        writer.WriteLine("end tell");
-            //        writer.WriteLine("EOF");
-            //        writer.Flush();
-            //    }
-
-            //var cmd = $"./{fileName}";
-
-            //$"chmod 700 {filePath}".Bash();
-            //var output = cmd.Bash();
-
-            #endregion
+            console.Connect();
 
             Console.ReadKey();
         }
@@ -88,30 +48,3 @@ namespace CoreApp
     }
 
 }
-
-
-
-//var appleScriptCmd = //"#!/bin/bash" +
-//        "osascript <<EOF" +
-//        "tell application \"System Events\"" +
-//        "tell current location of network preferences" +
-//            "set myVPN to the service \"UniVPN\"" +
-//            "if myVPN is not null then " +
-//            "if current configuration of myVPN is not connected then" +
-//                "connect myVPN" +
-//            "end if" +
-//            "end if" +
-//        "end tell" +
-//        "return 120 " +
-//        "end tell" +
-//        "EOF";
-
-//var appleScriptDemo = "osascript -e 'tell application \"Finder\"\ndisplay dialog \"Hello World\"\nend tell";
-
-//var ifconfigCmd = "ifconfig";
-//using (StreamWriter writer = new StreamWriter(fileName))
-//{
-//    writer.Write("Word ");
-//    writer.WriteLine("word 2");
-//    writer.WriteLine("Line");
-//}
